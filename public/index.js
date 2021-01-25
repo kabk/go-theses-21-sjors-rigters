bc040defd6d4f5bd36355c69b63d7c3a389d6ed1
Sjors Rigters <sjorsrigters@gmail.com>
2021-01-25 20:51:47 +0100

    Add files

--- public/index.js
+++ public/index.js
@@ -55,8 +55,184 @@ $(document).ready(function() {
   });
 
   $(document).ready(function() {
-    $('#bibliography-title').click(function() {
-      $('#bibliography-text').animate({
+    $('#existentialisme-title').click(function() {
+      $('#existentialisme-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#filmsovjetunie-title').click(function() {
+      $('#filmsovjetunie-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#gamegerelateerd-title').click(function() {
+      $('#gamegerelateerd-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#kolonialisme-title').click(function() {
+      $('#kolonialisme-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#minecraft-title').click(function() {
+      $('#minecraft-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#educatietool-title').click(function() {
+      $('#educatietool-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#emancipatietool-title').click(function() {
+      $('#emancipatietool-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#ontwerptool-title').click(function() {
+      $('#ontwerptool-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#virtuelesocialeruimte-title').click(function() {
+      $('#virtuelesocialeruimte-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#constructies-title').click(function() {
+      $('#constructies-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#feitenencijfers-title').click(function() {
+      $('#feitenencijfers-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#lego-title').click(function() {
+      $('#lego-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#notch-title').click(function() {
+      $('#notch-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#microsoft-title').click(function() {
+      $('#microsoft-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#redstonecomputer-title').click(function() {
+      $('#redstonecomputer-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#minecraftoverig-title').click(function() {
+      $('#minecraftoverig-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#landbouwenbioindustrie-title').click(function() {
+      $('#landbouwenbioindustrie-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#neoliberalisme-title').click(function() {
+      $('#neoliberalisme-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#roman-title').click(function() {
+      $('#roman-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#sanboxgame-title').click(function() {
+      $('#sandboxgame-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#simulatieargument-title').click(function() {
+      $('#simulatieargument-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#tomvdlinden-title').click(function() {
+      $('#tomvdlinden-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#overig-title').click(function() {
+      $('#overig-text').animate({
         height: "toggle"
       })
     });
---
+++ tailwind.config 2.js
@@ -0,0 +1,39 @@
+
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        25: '6.25rem',
+        25.5: '6.375rem',
+      },
+
+      colors: {
+        sun: '#ffffff',
+        sky: '#6eb4d2',
+        grass: '#009182',
+        dirt: '#624c43',
+        cobblestone: '#75737d',
+        gold: '#ffd801',
+        lava: '#f26827',
+        obsidian: '#46325c',
+        void: '#000000',
+      },
+
+      fontFamily: {
+        neue_haas_grotesk_ds: ["NeueHaasGroteskDisplay"],
+        neue_haas_grotesk_tx: ["NeueHaasGroteskText"],
+       },
+
+       fontSize: {
+        '5.5xl': ['3.375rem', { lineHeight: '1' }],
+       }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
