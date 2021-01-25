cf6c5a8f0064262a9154f9cd702336a1687bee17
Sjors Rigters <sjorsrigters@gmail.com>
2021-01-25 18:50:29 +0100

    Add files

---
+++ public/index.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+  $('#abstract-title').click(function() {
+    $('#abstract-text').animate({
+      height: "toggle"
+    })
+  });
+});
+
+$(document).ready(function() {
+    $('#intro-title').click(function() {
+      $('#intro-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#chapter1-title').click(function() {
+      $('#chapter1-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#chapter2-title').click(function() {
+      $('#chapter2-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#chapter3-title').click(function() {
+      $('#chapter3-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#chapter4-title').click(function() {
+      $('#chapter4-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#conclusion-title').click(function() {
+      $('#conclusion-text').animate({
+        height: "toggle"
+      })
+    });
+  });
+
+  $(document).ready(function() {
+    $('#bibliography-title').click(function() {
+      $('#bibliography-text').animate({
+        height: "toggle"
+      })
+    });
+  });
